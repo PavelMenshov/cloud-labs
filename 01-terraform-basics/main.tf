fda0c7496c5a95e44b92a7a8874671e2abca8a3e
PavelMenshov <kalabangaa58@gmail.com>
2026-06-23 17:44:34 +0800

    feat: updated main.tf file by adding sub-resource and variable blocks -this update is mainly used to understand how variables can be changed through CLI

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -1,6 +1,15 @@
 provider "random" {}
 
+variable "num_of_pets" {
+  type = number
+  description = "How many pets do we want"
+}
+
 resource "random_pet" "name" {
+  length = var.num_of_pets
+}
+
+resource "random_pet" "pauls-pet" {
   length = 3
 }
 
